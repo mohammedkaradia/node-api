3f4380841a973a8691cf01b545ef021b343c2ee2
mohammedkaradia <mohammedkaradia18@gmail.com>
2022-01-12 19:26:16 +0530

    hi

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,6 @@ router.get('/users', userController.getUser);
 router.get('/users/:id', userController.getUserById);
 
 router.post('/users',  [check('quoteemail', "your custom error message").isEmail(),
-                        check('quotenumber', "phone number not valid").isLength({ min: 10, max:10 })], userController.postNewUser);
+                        check('quotetel', "phone number not valid").isLength({ min: 10, max:10 })], userController.postNewUser);
 
 module.exports = router
